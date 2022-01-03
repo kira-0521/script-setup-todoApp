42d91c1fb97cb491b475eaf28c6433de7dd8425a
kira-0521 <kiratanaka0521@gmail.com>
2022-01-03 13:29:55 +0900

    エラー処理追加

--- src/views/todos.vue
+++ src/views/todos.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import AsyncTodosVue from '@/components/AsyncTodos.vue'
+import { onErrorCaptured, ref } from 'vue'
+
+const error = ref<unknown>(null)
+
+onErrorCaptured((err) => {
+  error.value = err
+  return true
+})
 </script>
 
 <template>
   <h2>Todo一覧です。</h2>
+  <div v-if="error">
+    {{ error.message }}
+  </div>
   <suspense>
     <template #default>
       <AsyncTodosVue />
